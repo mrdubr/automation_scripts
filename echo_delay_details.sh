6826bf20d75f0d59c7fa466d43fc667ce504a93c
Alexander Grachtchouk <agrachtchouk@pagerduty.com>
2022-04-18 14:49:40 -0400

    Update echo_delay_details.sh

--- echo_delay_details.sh
+++ echo_delay_details.sh
@@ -1,6 +1,7 @@
 
 #!/bin/bash
+echo "This is the V2 of this script"
 echo "Incident id: ${pd.incident.id}";
 echo "load_avg value: ${pd.alert.details.load_avg}";
 echo "ping_time value: ${pd.alert.details.ping_time}";
-echo "Done"
+echo "Done with v2 version"
